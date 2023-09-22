7ab7bf38dd9bf8d0f59d6c2f6f84ee71d0558720
Ceweiss42 <ceweiss7@gmail.com>
2023-09-21 23:15:41 -0700

    expanded projects page
    expanded projects page and made a youtube video auto-stop

--- js/projects.js
+++ js/projects.js
@@ -10,18 +10,26 @@ let tabs = ["1", "2", "3", "4"];
 
 function invis()
 {
-  document.getElementById("1").style.display = "none";
-  document.getElementById("2").style.display = "none";
-  document.getElementById("3").style.display = "none";
-  document.getElementById("4").style.display = "none";
+    document.getElementById("1").style.display = "none";
+    document.getElementById("2").style.display = "none";
+    document.getElementById("3").style.display = "none";
+    document.getElementById("4").style.display = "none";
   
 }
 
 function switchVisible(i)
 {
-  invis();
+    stopPlayer();
+    invis();
 
-  document.getElementById(i).style.display = "flex";
+    if (i == "2")
+    {
+        var iframe = document.querySelector('#youtube-video');
+        iframe.src = source;
+        console.log("HERE");
+    }
+  
+    document.getElementById(i).style.display = "flex";
 }
 
 function switchRight()
@@ -50,5 +58,14 @@ function switchLeft()
 
 }
 
+var source = "https://www.youtube.com/embed/XJI0XePPJJs?si=hqAaj4V6RwFlC8fN"; // Declare a global variable to hold the YouTube player
+
 
 window.onload = start;
+
+function stopPlayer() {
+    var iframe = document.querySelector('#youtube-video');
+    iframe.src = "";
+
+    console.log("stopPlayer() called")
+}
